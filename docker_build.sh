ce9c14e0eae7996c88a3196bb4aa0ab3a0710ed1
Ravil Galeyev <dehasi@yandex.ru>
2021-02-25 21:10:19 +0100

    Deploy

--- docker_build.sh
+++ docker_build.sh
@@ -1,13 +1,16 @@
 set -xue
 
+git submodule add git@github.com:dehasi/dehasi.github.io.git _site
+
 docker run --rm   --volume="$PWD:/srv/jekyll"   -it jekyll/jekyll   jekyll build
 
 cd _site/
-git init
+#git init
 git add .
-git remote add origin git@github.com:dehasi/dehasi.github.io.git
-git ci -am "Deploty"
-git push --set-upstream origin master --force
+#git remote add origin git@github.com:dehasi/dehasi.github.io.git
+git ci -am "Deploy"
+#git push --set-upstream origin master --force
+git push 
 
 cd ..
 rm -rf _site 
